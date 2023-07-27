0dd56688d5fa5329f1d7d290131062bf42490642
Dimitar Garkov <dimitar.garkov@uni-konstanz.de>
2023-07-28 01:34:30 +0200

    minor fixes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,14 +3,13 @@ import config from '../../config.json';
 
 export const getRepos = async () => {
   const { data } = await axios.get(
-    `https://api.github.com/users/${config.social.github}/repos?type=all&sort=pushed`,
-  );
+    `https://api.github.com/users/${config.social.github}/repos?type=all&sort=pushed`);
   return data;
 };
 
 export const getWeather = async (city: string) => {
   try {
-    const { data } = await axios.get(`https://wttr.in/${city}?ATm`, { crossdomain: true });
+    const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
     return data;
   } catch (error) {
     return error;
@@ -18,7 +17,7 @@ export const getWeather = async (city: string) => {
 };
 
 export const getQuote = async () => {
-  const { data } = await axios.get('https://api.quotable.io/random', { crossdomain: true });
+  const { data } = await axios.get('https://api.quotable.io/random');
   return {
     quote: `“${data.content}” — ${data.author}`,
   };
